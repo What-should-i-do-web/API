98642496dfba2955c3e43d76b250fa43b333b022
Melikehacibekiroglu <151880788+Melikehacibekiroglu@users.noreply.github.com>
2025-06-02 16:15:12 +0300

    Anasayfaya yönlendirme eklendi

---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-800 text-white text-3xl font-bold">
+      Ana Sayfa: Hoş geldin!
+    </div>
+  );
+}
+
+export default Home;
